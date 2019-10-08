24f62c6509e175b526a0893fae0c4179847fa8e4
unknown <antcala@icloud.com>
2019-10-08 15:53:02 -0500

    added 4 tests

--- Custom List Test/UnitTest1.cs
+++ Custom List Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Custom_List_Project;
 
 namespace Custom_List_Test
 {
@@ -7,8 +8,58 @@ namespace Custom_List_Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_AddToEmptyList_ItemGoesToIndexZero()
         {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 12;
+            int actual;
+
+            // act
+            testList.Add(12);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemToList_CountIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(234);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+
+            // unit test for adding multiple items to check position of last item
+            // unit test for adding multiple items to check Count property
+            // unit test for adding number of items beyond 'Capacity' but it still adds
+        }
+        [TestMethod]
+        public void Add_5ItemsToList()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 5;
+            int actual;
+
+            // act
+            testList.Add(67);
+            testList.Add(67);
+            testList.Add(67);
+            testList.Add(67);
+            testList.Add(67);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
